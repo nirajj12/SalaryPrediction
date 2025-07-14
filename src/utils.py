b16490ce43ead1cfc0cf89acbdf49092f9989737
nirajj12 <nirajofficial123@gmail.com>
2025-07-14 07:36:54 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,10 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 from src.exception import CustomException
+
 from src.logger import logging
 
 def save_object(file_path, obj):
@@ -19,3 +22,31 @@ def save_object(file_path, obj):
         logging.info(f"Object saved at {file_path}")
     except Exception as e:
         raise CustomException(e, sys) from e
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = accuracy_score(y_train, y_train_pred)
+            test_model_score = accuracy_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
